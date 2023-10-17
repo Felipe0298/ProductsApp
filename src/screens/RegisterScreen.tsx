d7ec75fd245e26d8f2f06208c50ff42e358132de
Felipe Hoyos Taborda <felipe02-98@hotmail.com>
2023-10-17 13:29:07 -0500

    321. Inicio de proyecto - ProductosApp

---
+++ src/screens/RegisterScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const RegisterScreen = () => {
+  return (
+    <View>
+      <Text>RegisterScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
