7d2cacade6f289e7d671f5bf81af22c0d9c52a0b
Felipe Hoyos Taborda <felipe02-98@hotmail.com>
2023-10-18 12:07:29 -0500

    333. Async Storage - Guardar Token

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { LoginData, LoginResponse, Usuario } from "../interfaces/appInterfaces";
 import { AuthState, authReducer } from "./AuthReducer";
 import cafeApi from "../api/cafeApi";
@@ -9,7 +10,7 @@ type AuthContextPorps = {
   user: Usuario | null;
   status: 'checking' | 'authenticated' | 'not-authenticated';
   signUp: () => void;
-  signIn: ( loginData: LoginData) => void;
+  signIn: (loginData: LoginData) => void;
   logOut: () => void;
   removeError: () => void;
 }
@@ -28,31 +29,49 @@ export const AuthProvider = ({ children }: any) => {
 
   const [state, dispatch] = useReducer(authReducer, authInitialState);
 
-  const signIn = async( {correo, password}: LoginData) => { 
+  useEffect(() => {
+    checkToken()
+  }, [])
+
+  const checkToken = async () => {
+    const token = AsyncStorage.getItem('token');
+
+    //No token, no autenticado
+    if (!token) return dispatch({ type: "notAuthenticated" })
+
+    //Hay token
+  }
+
+
+
+  const signIn = async ({ correo, password }: LoginData) => {
     try {
 
-      const { data } = await cafeApi.post<LoginResponse>('/auth/login', { correo, password});
+      const { data } = await cafeApi.post<LoginResponse>('/auth/login', { correo, password });
       dispatch({
-        type:'signUp',
-        payload:{
+        type: 'signUp',
+        payload: {
           token: data.token,
           user: data.usuario
         }
-      })
-      
+      });
+
+      await AsyncStorage.setItem('token', data.token)
+
     } catch (error: any) {
       console.log(error.response.data.msg)
-      dispatch({type:'addError',
-      payload: error.response.data.msg || 'Información incorrecta'
-    })
+      dispatch({
+        type: 'addError',
+        payload: error.response.data.msg || 'Información incorrecta'
+      })
     }
   };
 
   const signUp = () => { };
   const logOut = () => { };
 
-  const removeError = () => { 
-    dispatch({ type: 'removeError'})
+  const removeError = () => {
+    dispatch({ type: 'removeError' })
   };
 
 
