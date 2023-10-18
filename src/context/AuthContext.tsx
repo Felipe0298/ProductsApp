2a41322bb5ef098c76c0722a945a3ce77123adb7
Felipe Hoyos Taborda <felipe02-98@hotmail.com>
2023-10-18 10:23:27 -0500

    328. Conectar AuthContext con el authReducer

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,9 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 import { Usuario } from "../interfaces/appInterfaces";
+import { AuthState, authReducer } from "./AuthReducer";
 
-type AuthContextPorps={
-  errorMessage : string;
+type AuthContextPorps = {
+  errorMessage: string;
   token: string | null;
   user: Usuario | null;
   status: 'checking' | 'authenticated' | 'not-authenticated';
@@ -10,16 +11,31 @@ type AuthContextPorps={
   signIn: () => void;
   logOut: () => void;
   removeError: () => void;
-} 
+}
+
+
+const authInitialState: AuthState = {
+  status: "checking",
+  token: null,
+  user: null,
+  errorMessage: ''
+}
 
 export const AuthContext = createContext({} as AuthContextPorps);
 
-export const AuthProvider = ({children}: any) => {
+export const AuthProvider = ({ children }: any) => {
+
+  const [state, dispatch] = useReducer(authReducer, authInitialState);
+
+  const signUp = () => { };
+  const signIn = () => { };
+  const logOut = () => { };
+  const removeError = () => { };
 
 
-  return(
+  return (
     <AuthContext.Provider value={{
-      
+      ...state, signUp, signIn, logOut, removeError
     }}>
       {children}
     </AuthContext.Provider>
